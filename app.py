9e1bf98ec494dda4a349a7856e5cd8cc03cfb1e0
Chris Stevens <chris27stevens@gmail.com>
2015-04-02 16:31:43 +0100

    Working! Now working on deploying.

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 from websocket import WebSocketApp
 from ast import literal_eval
-from multiprocessing import Process, Manager
+import threading
 from flask import Flask, render_template
+from datetime import datetime
+from json import loads, dumps
 
 app = Flask(__name__)
 
 
-def socket_controller(dictionary):
-    print(dictionary)
+def socket_controller():
+    global button_data
+    try:
+        with open("lowest.json", "r") as f:
+            button_data = {"lowestTime": loads(f.read())}
+    except FileNotFoundError:
+        button_data = {"lowestTime": {"value": 60, "time": ""}}
 
     def received(socket, message):
-        message_dict = literal_eval(message)["payload"]
-        if message_dict["seconds_left"] <= dictionary["lowestTime"]["value"]:
-            print(message_dict["seconds_left"])
-            print("^^^^ LOWEST TIME!")
-            dictionary["lowestTime"]["value"] = message_dict["seconds_left"]
-            dictionary["lowestTime"]["time"] = message_dict["now_str"][-8:]
-
+        message_dict = literal_eval(message)["payload"]  # Convert string to dictionary and strip unneeded data.
+        if message_dict["seconds_left"] < button_data["lowestTime"]["value"]:
+            # Lowest Time
+            button_data["lowestTime"]["value"] = int(message_dict["seconds_left"])
+            button_data["lowestTime"]["time"] = message_dict["now_str"][-8:].replace("-", ":")
+            with open("lowest.json", "w") as f:
+                f.write(dumps(button_data["lowestTime"]))
+        button_data["clicks"] = int(message_dict["participants_text"].replace(",", ""))
+        button_data["clicks_second"] = round((button_data["clicks"] / (datetime.today() -
+                                              datetime(2015, 4, 1, 17, 00, 00)).total_seconds()), 3)
     socket = WebSocketApp("wss://wss.redditmedia.com/thebutton?h=18f357d4d3b377018523f3981d36f2c63f976873&e=1428054735",
                           on_message=received)
     socket.run_forever()
@@ -24,12 +34,8 @@ def socket_controller(dictionary):
 
 @app.route("/")
 def home():
-    print(d)
-    return render_template("home.html", data=d)
+    return render_template("home.html", data=button_data)
 
 if __name__ == "__main__":
-    manager = Manager()
-    d = manager.dict({"lowestTime": {"value": 60, "time": ""}})
-    websocket = Process(target=socket_controller, args=(d,))
-    websocket.start()
+    threading.Thread(target=socket_controller).start()
     app.run(debug=True, use_reloader=False)
