d7b97c7467b21b8f7586895771f7835e17b85dd8
Chris Stevens <chris27stevens@gmail.com>
2015-04-11 20:14:24 +0100

    [Dirty] Grey data is back! Also now including non-pressers.

--- soup.py
+++ soup.py
@@ -11,47 +11,54 @@ from secret import secret
 def five_minute():
     while True:
         # Get flair data.
-        flairs = {"colours": {"grey": 0, "purple": 0, "blue": 0, "green": 0, "yellow": 0, "orange": 0, "red": 0},
+        flairs = {"colours": {"grey": 0, "purple": 0, "blue": 0, "green": 0,
+                              "yellow": 0, "orange": 0, "red": 0, "cant": 0},
                   "counts": {}}
 
-        flair_data = requests.get("http://www.streamingflair.com/thebutton/rawthebutton2/").text
-        flair_soup = BeautifulSoup(flair_data).find(id="table1").find_all("tr")
-        flair_soup.pop(0)
+        flair_data = requests.get("http://www.reddit.com/r/thebutton/comments/31zag0/the_top_100/",
+                                  headers={"User-Agent": "/r/thebutton stats scraper (contact /u/Chr12t0pher)"}).text
+        flair_soup = BeautifulSoup(flair_data).find("div", {"class": "entry"}).find("div", {"class": "usertext-body"})
 
-        for i in range(1, 61):
-            flairs["counts"][i] = 0
+        soup_table = flair_soup.find("table").find_all("tr")
+        soup_table.pop(0)
 
-        for entry in flair_soup:
-            if entry.find_all("td")[1].text == "non presser":
-                pass  # Implementing this soon
-            elif entry.find_all("td")[1].text == "NoFlair":
-                pass  # Ignore users without flair.
-            elif entry.find_all("td")[1].text == "admin":
+        for entry in soup_table:
+            entry = entry.text.splitlines()
+            entry.pop(0)
+
+            if entry[0] == "non presser":
+                flairs["counts"]["non_presser"] = int(entry[1])
+                flairs["colours"]["grey"] = int(entry[1])
+            elif entry[0] == "Cowardly flair hiders":
+                flairs["counts"]["no_flair"] = int(entry[1])
+            elif entry[0] == "admin":
                 pass
-            elif entry.find_all("td")[1].text == "can't press":
-                pass  # Implementing this soon
+            elif entry[0] == "Noobs":
+                flairs["counts"]["cant_press"] = int(entry[1])
+                flairs["colours"]["cant"] = int(entry[1])
             else:
-                time = int(entry.find_all("td")[1].text[:-1])
-                flairs["counts"][time] += 1
+                flairs["counts"][entry[0][:-1]] = int(entry[1])
 
         for time in flairs["counts"]:
-            if 60 >= time > 50:
+            if time in ["non_presser", "cant_press", "no_flair"]:
+                pass
+            elif 60 >= int(time) > 50:
                 flairs["colours"]["purple"] += flairs["counts"][time]
-            elif 50 >= time > 41:
+            elif 50 >= int(time) > 41:
                 flairs["colours"]["blue"] += flairs["counts"][time]
-            elif 41 >= time > 31:
+            elif 41 >= int(time) > 31:
                 flairs["colours"]["green"] += flairs["counts"][time]
-            elif 31 >= time > 21:
+            elif 31 >= int(time) > 21:
                 flairs["colours"]["yellow"] += flairs["counts"][time]
-            elif 21 >= time > 11:
+            elif 21 >= int(time) > 11:
                 flairs["colours"]["orange"] += flairs["counts"][time]
-            elif 11 >= time > 0:
+            elif 11 >= int(time) > 0:
                 flairs["colours"]["red"] += flairs["counts"][time]
 
         with open(flairfile, "w") as f:
             f.write(dumps(flairs))
 
-        current_flair = {"purple": 0, "blue": 0, "green": 0, "yellow": 0, "orange": 0, "red": 0}
+        current_flair = {"purple": 0, "blue": 0, "green": 0, "yellow": 0, "orange": 0, "red": 0, "grey": 0, "cant": 0}
 
         for flair in flairs["colours"]:
             current_flair[flair] = round(float((flairs["colours"][flair] / sum(flairs["colours"].values())) * 100), 2)
