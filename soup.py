ba64abd68f8eb5cb1463dd11c4370ad82db09c6a
Chris Stevens <chris27stevens@gmail.com>
2015-04-22 20:11:19 +0100

    More accurate flair data! Also, I really need to refactor this, the code is shocking...

--- soup.py
+++ soup.py
@@ -4,11 +4,10 @@ from time import sleep
 from app import flairfile, currentflairfile
 from json import dumps
 import threading
-import praw
-from secret import secret
+import csv
 
 
-def five_minute():
+def two_minute():
     while True:
         # Get flair data.
         flairs = {"colours": {"grey": 0, "purple": 0, "blue": 0, "green": 0,
@@ -42,9 +41,9 @@ def five_minute():
         for time in flairs["counts"]:
             if time in ["non_presser", "cant_press", "no_flair"]:
                 pass
-            elif 60 >= int(time) > 50:
+            elif 60 >= int(time) > 51:
                 flairs["colours"]["purple"] += flairs["counts"][time]
-            elif 50 >= int(time) > 41:
+            elif 51 >= int(time) > 41:
                 flairs["colours"]["blue"] += flairs["counts"][time]
             elif 41 >= int(time) > 31:
                 flairs["colours"]["green"] += flairs["counts"][time]
@@ -55,6 +54,36 @@ def five_minute():
             elif 11 >= int(time) > 0:
                 flairs["colours"]["red"] += flairs["counts"][time]
 
+        # Get 'actual' flair data.
+        actual_flair_data_csv = requests.get("http://tcial.org/the-button/button_clicks.csv").content.decode().split("\n")
+        actual_flair_data = {}
+        actual_flair_data_csv.pop(0)
+        actual_flair_data_csv.pop(-1)
+        for i in range(1, 61):
+            actual_flair_data[str(i)] = 0
+        for row in csv.reader(actual_flair_data_csv):
+            actual_flair_data[row[2]] += int(row[1])
+
+        flairs["calc_flairs"] = {"red": 0, "orange": 0, "yellow": 0, "green": 0, "blue": 0, "purple": 0}
+
+        for time in actual_flair_data:
+            if 60 >= int(time) > 50:
+                flairs["calc_flairs"]["purple"] += actual_flair_data[time]
+            elif 51 >= int(time) > 41:
+                flairs["calc_flairs"]["blue"] += actual_flair_data[time]
+            elif 41 >= int(time) > 31:
+                flairs["calc_flairs"]["green"] += actual_flair_data[time]
+            elif 31 >= int(time) > 21:
+                flairs["calc_flairs"]["yellow"] += actual_flair_data[time]
+            elif 21 >= int(time) > 11:
+                flairs["calc_flairs"]["orange"] += actual_flair_data[time]
+            elif 11 >= int(time) > 0:
+                flairs["calc_flairs"]["red"] += actual_flair_data[time]
+
+        flairs["current_calc_flairs"] = {"purple": 0, "blue": 0, "green": 0, "yellow": 0, "orange": 0, "red": 0}
+        for flair in flairs["calc_flairs"]:
+            flairs["current_calc_flairs"][flair] = round(float((flairs["calc_flairs"][flair] / sum(actual_flair_data.values())) * 100), 2)
+        
         with open(flairfile, "w") as f:
             f.write(dumps(flairs))
 
@@ -66,8 +95,9 @@ def five_minute():
         with open(currentflairfile, "w") as f:
             f.write(dumps(current_flair))
 
-        sleep(300)
+
+        sleep(120)
 
 
 if __name__ == "__main__":
-    threading.Thread(target=five_minute).start()
+    threading.Thread(target=two_minute).start()
